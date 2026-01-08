a0ea2eb17e7f300a8d20995ccaf13f679fd3cbd1
DeveloperBhin <norbeth559@gmail.com>
2026-01-08 09:45:24 +0300

    modify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SocketProvider } from '@/context/SocketContext';
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { SocketProvider } from "@/context/SocketContext";
 
 export const metadata: Metadata = {
   title: "UNO Card Game",
@@ -20,17 +9,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-   <html lang="en">
-      <body>
+    <html lang="en">
+      <body className="font-sans">
         <SocketProvider>{children}</SocketProvider>
       </body>
     </html>
   );
 }
-
-
-
