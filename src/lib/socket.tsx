f1240ef7066f7045baa46f3403b0dc1eb41280cf
DeveloperBhin <norbeth559@gmail.com>
2026-01-08 10:15:06 +0300

    modify

---
+++ src/lib/socket.tsx
@@ -0,0 +1,15 @@
+import { io, Socket } from "socket.io-client";
+
+let socket: Socket | null = null;
+
+export function getSocket() {
+  if (!socket) {
+    socket = io(process.env.NEXT_PUBLIC_SOCKET_URL! || 'https://yit-apis.onrender.com', {
+      transports: ["websocket"],
+      autoConnect: true,
+      reconnection: true,
+      reconnectionAttempts: Infinity,
+    });
+  }
+  return socket;
+}
